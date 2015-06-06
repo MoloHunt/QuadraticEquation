dfc10b156fbc0e9ffd7f6336c6835bea2d54e7ca
Brennan Forrest <doktuhparadox@gmail.com>
2015-06-06 00:17:59 -0500

    This is a simple change.
    You can name constructor parameters and class fields the same way so long as you reference the class field with its name prefixed with "this." if necessary.
    
    For example:
    
    public class C {
        private int i;
    
        public C (int i) {
            this.i = i; //Set the class field "i" to the constructor paremeter "i"; note how they share a name but not scope
        }
    }
    
    Read more about the usage of "this" here: https://docs.oracle.com/javase/tutorial/java/javaOO/thiskey.html

--- src/MathematicalBackend/ComplexNumber.java
+++ src/MathematicalBackend/ComplexNumber.java
@@ -12,10 +12,10 @@ public class ComplexNumber {
     private float real; //represents real component
     private float unreal; //The unreal component not the game engine
 
-    //Constructor, uses a + bi form for parameters
-    public ComplexNumber(float a, float b){
-        real = a;
-        unreal = b;
+    //Constructor, uses real + bi form for parameters
+    public ComplexNumber(float real, float unreal){
+        this.real = real;
+        this.unreal = unreal;
     }
 
     //Allows you to add two complex numbers, and by that you can also subtract
@@ -30,7 +30,7 @@ public class ComplexNumber {
 
     //allows you to get any complex number as a sting in the form a + bi
     public static String ToString(ComplexNumber compNum){
-        return compNum.getReal() + ((compNum.getUnreal() >= 0)? " + " : " ") + compNum.getUnreal() + "i";
+        return compNum.getReal() + ((compNum.getUnreal() >= 0) ? " + " : " ") + compNum.getUnreal() + "i";
     }
 
     public float getReal(){
